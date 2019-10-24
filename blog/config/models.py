f3216f716da5a2b15c7b178e2ab0a0a0acbc08c7
shiretree <2524619715@qq.com>
2019-10-24 15:47:41 +0800

    第二次将app和model提交到主分支.

---
+++ blog/config/models.py
@@ -0,0 +1,54 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+class Link(models.Model):
+
+    STATUS_NORMAL = 1           #为啥子都要个状态呢？
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+    )
+
+    title = models.CharField(max_length=50, verbose_name="标题")
+    href = models.URLField(verbose_name="链接")   #默认长度为200
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    weight = models.PositiveIntegerField(default=1, choices=zip(range(1,6),range(1,6)), verbose_name="权重",
+                                         help_text="权重高的展示序列靠前")   #为什么两个range？查一下zip用法
+    """
+    当zip()函数有两个参数时，zip(a,b)函数分别从a和b中取一个元素组成元组，再次将组成的元组组合成一个新的迭代器。
+    a与b的维数相同时，正常组合对应位置的元素。当a与b行或列数不同时，取两者中的最小的行列数。
+    """
+    owner = models.ForeignKey(User, verbose_name="作者")
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = '友链'
+
+class SideBar(models.Model):
+
+    STATUS_SHOW = 1
+    STATUS_HIDE = 0
+    STATUS_ITEMS = (
+        (STATUS_SHOW, '展示'),
+        (STATUS_HIDE, '隐藏'),
+    )
+
+    SIDE_TYPE = (
+        (1, 'HTML'),
+        (2, '最新文章'),
+        (3, '最热文章'),
+        (4, '最热评论'),
+    )
+
+    title = models.CharField(max_length=50, verbose_name="标题")
+    display = models.PositiveIntegerField(default=1, choices=SIDE_TYPE,verbose_name="展示类型")
+    content = models.CharField(max_length=500, blank=True, verbose_name="内容", help_text="如果设置的不是HTML类型，可为空")
+    status = models.PositiveIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS, verbose_name="状态")
+    owner = models.ForeignKey(User, verbose_name="作者")
+    created_time =  models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "侧边栏"
